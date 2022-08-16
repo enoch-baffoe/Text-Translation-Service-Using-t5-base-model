4bae1dbc5d733f736f698845797768062a7286e8
Enoch P Baffoe <bafpep@gmail.com>
2022-08-16 19:21:04 +0000

    feat: Adding translator routes  These are the routes that use the translation model which has been saved in the model folder to translate inputs from the /translate endpoint

---
+++ api/v1/translate.py
@@ -0,0 +1,57 @@
+from fastapi import APIRouter, status
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+from schemes.translate import Languages
+
+
+translation_router = APIRouter(tags=["Translate"])
+
+
+@translation_router.on_event("startup")
+def load_model():
+    """
+    This loads the tokenizer and model on startup.
+    The model and tokenizer loaded here is the one saved in the models folder
+    """
+    global model
+    global tokenizer
+    model = T5ForConditionalGeneration.from_pretrained("./models")
+    tokenizer = T5Tokenizer.from_pretrained("./models")
+
+
+@translation_router.post("/translate", status_code=status.HTTP_200_OK)
+async def translate(
+    source_language: Languages,
+    destination_language: Languages,
+    input_text: str,
+):
+    """
+    Translates text among 4 different languages namely:
+    English, French Romanian, German
+
+    - Inputs:
+        - source_language: the language your text is in
+        - destination_language: the language you want to translate the text to
+        - Input_text: the text you want to translate
+
+
+    - Output:
+        - success: boolean to show if text was successfully translated
+        - translated_text: the translated text from the algorithm
+    """
+    input_ids = tokenizer(
+        "translate "
+        + source_language.value
+        + " to "
+        + destination_language.value
+        + ": "
+        + input_text,
+        return_tensors="pt",
+    ).input_ids
+
+    outputs = model.generate(input_ids)
+
+    translated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    if translated_text:
+        return {"success": True, "translated_text": translated_text}
+    return {"success": False, "translated_text": None}
